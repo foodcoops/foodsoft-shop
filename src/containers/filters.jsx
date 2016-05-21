536e799f67697d08d6f1ba3ebfd00c774b6680a0
wvengen <dev-voko@willem.engen.nl>
2016-05-22 01:56:45 +0200

    Add order close date to filters

--- src/containers/filters.jsx
+++ src/containers/filters.jsx
@@ -1,13 +1,28 @@
 import React, {PropTypes} from 'react';
-import {Accordion, ListGroup, ListGroupItem, Panel} from 'react-bootstrap';
+import {Accordion, Badge, Glyphicon, ListGroup, ListGroupItem, OverlayTrigger, Panel, Tooltip} from 'react-bootstrap';
 
+import moment from 'moment';
 import {connect} from 'react-redux';
 import rest from '../store/rest';
 import filter from '../store/filter';
 import SearchBox from '../components/search_box';
 
-import {t} from 'i18n';
+import {t, l} from 'i18n';
 const T = (s, opts) => t('filters.'+s, opts);
+
+function getOrderName(order) {
+  const days = order.ends ? Math.round(moment(order.ends).diff(moment(), 'days', true)) : null;
+  const tooltip = <Tooltip id={'filter-order-'+order.id}>{T('order_open_until', {ends: moment(order.ends).format('lll')})}</Tooltip>;
+  return (
+    <span>
+      {order.name}
+      {(days >= 0) ?
+        <OverlayTrigger placement='right' overlay={tooltip}>
+          <small style={styles.ends}>{days > 0 ? T('days_short', {count: days}) : <Glyphicon glyph='time' />}</small>
+        </OverlayTrigger> : null}
+    </span>
+  );
+}
 
 class Filters extends React.Component {
 
@@ -28,19 +43,19 @@ class Filters extends React.Component {
               // @todo move knowledge of search param key to rest.js
               this._renderPanel('article_article_category_id', i += 1, T('categories'), this.props.categories.data.data) : null}
           {this.hasOrders() ?
-              this._renderPanel('order_id', i += 1, T('orders'), this.props.orders.data.data) : null}
+              this._renderPanel('order_id', i += 1, T('orders'), this.props.orders.data.data, getOrderName) : null}
         </Accordion>
       </div>
     );
   }
 
-  _renderPanel(id, key, title, items) {
+  _renderPanel(id, key, title, items, getName = (i) => i.name) {
     return (
       <Panel eventKey={key} header={title}>
         <ListGroup fill>
           {items.map((item) => (
             <ListGroupItem key={item.id} href='#' active={this.props.filter[`${id}_eq`] === item.id}
-                           onClick={this._onClick.bind(this, id, item.id)}>{item.name}</ListGroupItem>
+                           onClick={this._onClick.bind(this, id, item.id)}>{getName(item)}</ListGroupItem>
           ))}
         </ListGroup>
       </Panel>
@@ -77,5 +92,9 @@ const styles = {
   searchBox: {
     marginBottom: 5,
     border: 'none', // for panel-like appearence, input already has border
+  },
+  ends: {
+    marginLeft: '0.5em',
+    opacity: 0.75,
   }
 };
