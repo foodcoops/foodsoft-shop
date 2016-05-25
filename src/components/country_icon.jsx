2e89f900c4a63092c947bcdf3a59df74de9535da
wvengen <dev-voko@willem.engen.nl>
2016-05-25 23:11:26 +0200

    Fix unrecognised country icon being shown

--- src/components/country_icon.jsx
+++ src/components/country_icon.jsx
@@ -3,13 +3,6 @@ import React from 'react';
 // needs to have flag-icon-css stylesheet loaded
 
 const CountryIcon = ({code}) => (code && code.length==2) ?
-  <div className={`famfamfam-flags ${code.toLowerCase()}`} style={styles.icon} /> : null;
-
-const styles = {
-  icon: {
-    width: 14,
-    height: 11,
-  }
-}
+  <div className={`famfamfam-flags ${code.toLowerCase()}`} /> : null;
 
 export default CountryIcon;
