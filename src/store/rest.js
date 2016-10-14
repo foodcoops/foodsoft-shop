3c29c0eb085ef10ff96c8e4bf7333d0032a99878
wvengen <dev-voko@willem.engen.nl>
2016-10-14 21:44:06 +0200

    Move group_order_articles update intelligence to helper

--- src/store/rest.js
+++ src/store/rest.js
@@ -17,7 +17,7 @@ function options(url, params, getState) {
   return {headers: headers};
 };
 
-export default reduxApi({
+const rest = reduxApi({
   user: {
     url: '/api/v1/user'
   },
@@ -37,8 +37,36 @@ export default reduxApi({
   group_order_articles: {
     url: '/api/v1/group_order_articles/(:id)',
     transformer: icrud.transformer,
-    helpers: icrud.helpers
+    helpers: {
+      change(oa, goa, data) {
+        const goa_q = data.quantity !== undefined ? data.quantity : (goa || {}).quantity;
+        const goa_t = data.tolerance !== undefined ? data.tolerance : (goa || {}).tolerance;
+
+        // @todo perhaps change api to only work with order_article_id and get rid of group_order_articles
+        if (goa) {
+          if (goa_q === 0 && goa_t === 0) {
+            // amounts are zero, destroy it
+            // (we could update it too, but the response would be that it's deleted, we can't understand that response from update)
+            return icrud.helpers.destroy(goa.id, () => {
+              this.dispatch(rest.actions.order_articles.get(goa.order_article_id));
+            });
+          } else {
+            // update existing group_order_article
+            return icrud.helpers.update(goa.id, data, (error) => {
+              error || this.dispatch(rest.actions.order_articles.get(goa.order_article_id));
+            });
+          }
+        } else if (goa_q > 0 || goa_t > 0) {
+          // no existing group_order_article, create one
+          return icrud.helpers.create({...data, order_article_id: oa.id}, (error) => {
+            error || this.dispatch(rest.actions.order_articles.get(oa.id));
+          });
+        }
+      }
+    },
   }
 }).use('fetch', restFetch(fetch))
   .use('options', options)
   .use('rootUrl', foodsoftUrl);
+
+export default rest;
