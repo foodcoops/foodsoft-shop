5fb3cdad5ed8bd4a485b9aae2d338c06be80733a
wvengen <dev-voko@willem.engen.nl>
2018-10-12 22:52:08 +0200

    Add page footer

--- src/containers/layout.jsx
+++ src/containers/layout.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { Button, Clearfix, DropdownButton, MenuItem, Navbar, NavDropdown, Nav, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
-import { foodsoftUrl } from '../config';
+import { foodsoftUrl, appVersion } from '../config';
 import { fetchCurrentUser } from '../actions/user';
 import { fetchConfig } from '../actions/config';
 import Notifications from '../containers/notifications';
@@ -26,28 +26,38 @@ class Layout extends React.Component {
     return (
       <div>
         <Notifications />
-        <div className='nav-pre-header'>
-          <div className='logo pull-left'>
-            <a href={foodsoftUrl}><Logo /></a>
+        <header>
+          <div className='nav-pre-header'>
+            <div className='logo pull-left'>
+              <a href={foodsoftUrl}><Logo /></a>
+            </div>
+            <Nav bsStyle='pills' className='nav-pre-buttons' pullRight>
+              <NavDropdown title={user.name} id='user-dropdown'>
+                <MenuItem href={`${foodsoftUrl}/home/profile`} eventKey='1'>{T('profile')}</MenuItem>
+                <MenuItem href={`${foodsoftUrl}/home/ordergroup`} eventKey='2'>{T('ordergroup')}</MenuItem>
+                <MenuItem href={`${foodsoftUrl}/logout`} eventKey='3'>{T('logout')}</MenuItem>
+              </NavDropdown>
+              <NavItem href={config.homepage}>{config.name}</NavItem>
+              {config.help_url ? <NavItem href={config.help_url}>{T('help')}</NavItem> : null}
+              <NavItem href={`${foodsoftUrl}/feedback/new`}>{T('feedback')}</NavItem>
+            </Nav>
+            <Clearfix />
           </div>
-          <Nav bsStyle='pills' className='nav-pre-buttons' pullRight>
-            <NavDropdown title={user.name} id='user-dropdown'>
-              <MenuItem href={`${foodsoftUrl}/home/profile`} eventKey='1'>{T('profile')}</MenuItem>
-              <MenuItem href={`${foodsoftUrl}/home/ordergroup`} eventKey='2'>{T('ordergroup')}</MenuItem>
-              <MenuItem href={`${foodsoftUrl}/logout`} eventKey='3'>{T('logout')}</MenuItem>
-            </NavDropdown>
-            <NavItem href={config.homepage}>{config.name}</NavItem>
-            {config.help_url ? <NavItem href={config.help_url}>{T('help')}</NavItem> : null}
-            <NavItem href={`${foodsoftUrl}/feedback/new`}>{T('feedback')}</NavItem>
-          </Nav>
-          <Clearfix />
-        </div>
-        <Navbar fluid>
-          <Navigation />
-        </Navbar>
-        <div className="container-fluid">
+          <Navbar fluid>
+            <Navigation />
+          </Navbar>
+        </header>
+        <main className="container-fluid">
           {this.props.children}
-        </div>
+        </main>
+        <footer class="container-fluid">
+          <div className="pull-right">
+            {(config.foodsoft && config.foodsoft.url) ? <a href={config.foodsoft.url}>{t('layout.foodsoft')}</a> : t('layout.foodsoft') }
+            {(config.foodsoft && config.foodsoft.version) && ` v${config.foodsoft.version} +`}
+            {` ${t('layout.foodsoft_shop_short')} v${appVersion}`}
+          </div>
+          {config.page_footer_html && <div dangerouslySetInnerHTML={{ __html: config.page_footer_html }} />}
+        </footer>
       </div>
     );
   }
